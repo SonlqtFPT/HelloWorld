6991de4b94f5b8cea67788adc47c9577cf8ba340
sonlqtFPT <sonlqtse184097@fpt.edu.vn>
2024-09-19 15:49:51 +0700

    test 1st commit

---
+++ seminar_reactjs-main/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./page/home";
+import Login from "./page/login";
+import Dashboard from "./page/admin";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "admin",
+      element: <Dashboard />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
+
+// Single Page Application
+// chỉ sử dụng 1 trang duy nhất
+//khi chuyển trang => thay đổi cái nội dung bên trong trang web
